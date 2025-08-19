bf3c74903e402151a737aa3068ca2de8e0a1f02e
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-08-19 01:50:38 -0400

    test: add sample test and configure Jest for Node environment

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@
 module.exports = async () => {
 	return {
 	  verbose: true,
+	  testEnvironment: 'node',
+	  testMatch: ['**/__tests__/**/*.{js,jsx,ts,tsx}', '**/*.{test,spec}.{js,jsx,ts,tsx}'],
 	};
   };
